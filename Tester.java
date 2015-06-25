56e9a866fbc2cd678fe9bdc48d03040f070a9321
Netanel Draiman <ndraiman@gmail.com>
2015-06-25 22:16:32 +0300

    frame will open in middle of screen

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 package ex5;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -7,21 +10,28 @@ public class Tester {
 	
 	public static void main(String[] args) {
 		
-		JOptionPane.showConfirmDialog(null, "Tester Created by Netanel Draiman", "FlightDatabase Tester",
+		JOptionPane.showConfirmDialog(null, "Created by Netanel Draiman \nTester v1.0", "FlightDatabase Tester",
 				JOptionPane.CLOSED_OPTION, JOptionPane.INFORMATION_MESSAGE);
 		
 		JFrame frame = new JFrame("FlightsDatabase");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
 		
+		//Open Frame in middle of screen
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+		
+		//Output Redirection - Yes = Frame, No = Console.
 		int option = JOptionPane.showConfirmDialog(null, "Redirect Output to Frame? \n (No = Output to Console)",
 				"Output Redirect", JOptionPane.YES_NO_OPTION);
 		
-		//Put your SQL Username & Password into the Constructor below
-		FlightsPanel fp = new FlightsPanel("system", "skywalker", option);
+		/*******************************************************************/
+		/*** Put your SQL Username & Password into the Constructor below ***/
+		/*******************************************************************/
+		FlightsPanel fp = new FlightsPanel("system", "password", option);
 		frame.add(fp);
-		frame.setVisible(true);
-		frame.setSize(500, 500);
 		
+		frame.setVisible(true);
 		
 	}
 
